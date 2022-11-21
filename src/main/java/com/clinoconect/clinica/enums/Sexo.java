d85717fa7a58bdaffa9fc2a180cabbbb435f0365
Lara Menck <lara.menckt@gmail.com>
2022-11-21 14:39:53 -0300

    codigos enum

--- src/main/java/com/clinoconect/clinica/enums/Sexo.java
+++ src/main/java/com/clinoconect/clinica/enums/Sexo.java
@@ -2,7 +2,26 @@ package com.clinoconect.clinica.enums;
 
 public enum Sexo {
 
-	FEMININO,
-	MASCULINO
+	FEMININO(0),
+	MASCULINO(1);
+	
+	private int code;
+
+	private Sexo(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public static Sexo valueOf(int code) {
+		for (Sexo value : Sexo.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Codigo invalido");
+	}
 	
 }
